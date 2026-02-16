9e0c4f66701198ffcdb766d53935e81a901c385b
Abhiram678 <141011060+Abhiram678@users.noreply.github.com>
2026-02-16 10:52:38 +0530

    Add HelloJenkins class with main method and addition

---
+++ HelloJenkins.java
@@ -0,0 +1,20 @@
+public class HelloJenkins {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, Jenkins CI/CD Pipeline!");
+        System.out.println("This project is pushed to GitHub and built using Jenkins.");
+
+        int result = add(5, 3);
+        System.out.println("Addition Result: " + result);
+
+        if(result == 8){
+            System.out.println("Build Success ✅");
+        } else {
+            System.out.println("Build Failed ❌");
+        }
+    }
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+}
